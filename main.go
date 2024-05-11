c7091544b4ad6ebdd082fbfc89c6fd5d41311487
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-11 20:36:17 +0900

    go mod init

--- main.go
+++ main.go
@@ -5,4 +5,7 @@ import "fmt"
 func main(){
 	fmt.Println("Welcome to our conference booking aoolication")
 	fmt.Println("Get your tickets here to attend")
+
+	var conferenceName = "Go Conference"
+	fmt.Println("Event Name:", conferenceName)
 }
