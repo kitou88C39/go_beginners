f9b64cc8fe5e29c1e8b02efea98b65bf5e2a6c8d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-11 20:59:55 +0900

    go mod init

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ func main() {
 	var remainingTickets = 50
 
 	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
-
-	
 
 }
