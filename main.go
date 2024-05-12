f702512d839a86c1e2caed61ef75e2763dfedfa5
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-12 22:00:17 +0900

    go mod init

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceTickets)
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
