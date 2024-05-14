b4e8d4eaa77df346772b36061d80223c9a81e926
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-14 09:52:22 +0900

    len

--- main.go
+++ main.go
@@ -33,6 +33,8 @@ for  {
 	fmt.Println("Enter number of tickets:")
 	fmt.Scan(&userTickets)
 
+	len(firstName) >= 2
+
 	if userTickets < uint(remainingTickets) {
 		remainingTickets = remainingTickets - int(userTickets)
 		bookings = append(bookings, firstName + " " + lastName)
