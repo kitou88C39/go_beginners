5f249f1dfa1f67c0d2df3000cfb177d5d939f9aa
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-15 16:47:10 +0900

    returning multiple values

--- main.go
+++ main.go
@@ -14,23 +14,7 @@ func main() {
 	greetUsers(conferenceName, conferenceTickets, uint(remainingTickets))
 	
 	for  {
-		// var firstName string
-		// var lastName string
-		// var email string
-		// var userTickets uint
 		
-		// fmt.Println("Enter your first name:")
-		// fmt.Scan(&firstName)
-		
-		// fmt.Println("Enter your last name:")
-		// fmt.Scan(&lastName)
-		
-		// fmt.Println("Enter your  email address:")
-		// fmt.Scan(&email)
-		
-		// fmt.Println("Enter number of tickets:")
-		// fmt.Scan(&userTickets)
-
 		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTicket, remainingTickets)
 		
 		if  isValidName &&  isValidEmail &&  isValidTicketNumber {
@@ -54,8 +38,8 @@ func main() {
 					if !isValidEmail {
 						fmt.Println("email address you entered doesn't contain @ sign")
 						} 
-					if !isValidTicketNumber {
-						fmt.Println("number of tickets you entered is invalid")
+						if !isValidTicketNumber {
+							fmt.Println("number of tickets you entered is invalid")
 						}
 					}
 				}
@@ -75,10 +59,29 @@ func main() {
 				}
 				return firstNames
 			}
-
-			func validateUserInput(firstName string, lastName string, email string, userTicket uint,remainingTickets uint)(bool,bool,bool){
+			
+			func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
 				isValidName := len(firstName) >= 2 && len(lastName) >= 2
 				isValidEmail := strings.Contains(email, "@")
 				isValidTicketNumber := userTickets > 0 && userTickets <= uint(remainingTickets)
 				return isValidName, isValidEmail, isValidTicketNumber
+			}
+			
+			func greetUserInput(){
+				var firstName string
+				var lastName string
+				var email string
+				var userTickets uint
+				
+				fmt.Println("Enter your first name:")
+				fmt.Scan(&firstName)
+				
+				fmt.Println("Enter your last name:")
+				fmt.Scan(&lastName)
+				
+				fmt.Println("Enter your  email address:")
+				fmt.Scan(&email)
+				
+				fmt.Println("Enter number of tickets:")
+				fmt.Scan(&userTickets)
 			}
