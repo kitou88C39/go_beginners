32e738071e3fd73a79179cca80dbbbbaf57a87e1
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-17 16:51:08 +0900

    構造体 + メソッド

--- go_sample/structure.go
+++ go_sample/structure.go
@@ -6,8 +6,11 @@ type Rectangle struct  {
 	width, height int
 }
 
+func (r Rectangle) Area() float64 {
+	return float64(r.width * r.height)
+}
+
 func structure (){
 	r := Rectangle{width:20, height:30}
 	fmt.Println(r)
-	fmt.Println(r.height)
 }
