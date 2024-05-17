f631b06328f0ee8f9535428657f1c39d61168f1a
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-17 16:48:16 +0900

    構造体 + メソッド

--- go_sample/structure.go
+++ go_sample/structure.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
 type Rectangle struct  {
 	width, height int
 }
 
 func structure (){
-	
+	r := Rectangle{width:20, height:30}
+	fmt.Println(r)
+	fmt.Println(r.height)
 }
